9bff4725a1a947c60182f1ddd98c6f9f8eab51a2
jean-carlos <jeancarlos_maia@hotmail.com>
2022-02-02 14:20:32 -0300

    feat: fake api for dev env

--- src/lib/fetchVideos.ts
+++ src/lib/fetchVideos.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 import serializeVideosData from '../utils/serializeVideosData';
+import fakeData from '../../videos.dev.json';
 
 export default async function fetchVideos(params: string) {
+  if (process.env.NEXT_PUBLIC_ENV === 'development') {
+    return serializeVideosData(fakeData);
+  }
   try {
     const ytApiKey = process.env.YOUTUBE_API_KEY;
     const baseUrl = 'https://youtube.googleapis.com/youtube/v3';
