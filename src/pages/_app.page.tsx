9a5aa8eb26220a5f19249b8d18336782ea535529
jean-carlos <jeancarlos_maia@hotmail.com>
2022-02-02 14:48:39 -0300

    chore: progress bar when loading a page

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../styles/theme';
+import NextNprogress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
+      <NextNprogress color="#E53E3E" />
       <Component {...pageProps} />
     </ChakraProvider>
   );
