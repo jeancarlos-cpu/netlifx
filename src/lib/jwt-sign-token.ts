05807267b5e7360043985428b29d2e752a7a46f6
jean-carlos <jeancarlos_maia@hotmail.com>
2022-02-03 20:13:21 -0300

    fix: jwt payload

--- src/lib/jwt-sign-token.ts
+++ src/lib/jwt-sign-token.ts
@@ -1,20 +1,19 @@
 import jwt from 'jsonwebtoken';
 
 export default function JwtSignToken(issuer: string) {
-  const sevenDaysInSeconds = 7 * 24 * 60 * 60;
-
   const token = jwt.sign(
     {
-      payload: {
-        issuer,
+      issuer,
+      'https://hasura.io/jwt/claims': {
         'x-hasura-allowed-roles': ['user', 'admin'],
         'x-hasura-default-role': 'user',
         'x-hasura-user-id': `${issuer}`,
-        iat: Math.floor(Date.now() / 1000),
-        exp: Math.floor(Date.now() / 1000 + sevenDaysInSeconds),
       },
     },
     String(process.env.JWT_SECRET),
+    {
+      expiresIn: '7d',
+    },
   );
 
   return token;
