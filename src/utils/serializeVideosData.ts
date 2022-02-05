74e38b8a2e85fc5155ec599bd30737f4849a0f29
jean-carlos <jeancarlos_maia@hotmail.com>
2022-02-04 21:28:55 -0300

    feat: better video thumbnail quality

--- src/utils/serializeVideosData.ts
+++ src/utils/serializeVideosData.ts
@@ -1,11 +1,15 @@
 export default function serializeVideosData(data: VideosData) {
-  return data.items.map((video) => ({
-    imgUrl: video.snippet.thumbnails.high.url,
-    id: video.id.videoId ? video.id.videoId : video.id,
-    title: video.snippet.title,
-    description: video.snippet.description,
-    publishedAt: video.snippet.publishedAt,
-    channelTitle: video.snippet.channelTitle,
-    viewCount: video?.statistics?.viewCount ?? 0,
-  }));
+  return data.items.map((video) => {
+    const id = video.id.videoId ? video.id.videoId : video.id;
+    const imgUrl = `https://i.ytimg.com/vi/${id}/maxresdefault.jpg`;
+    return {
+      id,
+      imgUrl,
+      title: video.snippet.title,
+      description: video.snippet.description,
+      publishedAt: video.snippet.publishedAt,
+      channelTitle: video.snippet.channelTitle,
+      viewCount: video?.statistics?.viewCount ?? 0,
+    };
+  });
 }
